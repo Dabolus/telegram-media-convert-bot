c2e561293ac03608d4998df255b0072583ddbff8
Giorgio Garasto <giorgio@garasto.it>
2021-01-03 13:17:36 +0100

    refactor: simplify bot entrypoint

--- src/image.ts
+++ src/image.ts
@@ -1,20 +1,64 @@
+import TelegramBot from 'node-telegram-bot-api';
 import sharp from 'sharp';
 
-export const imageToSticker = async (fileBuffer: Buffer) =>
-  sharp(fileBuffer)
-    .resize({
-      width: 512,
-      height: 512,
-      fit: 'contain',
-      background: {
-        r: 0,
-        g: 0,
-        b: 0,
-        alpha: 0,
-      },
-    })
-    .webp()
-    .toBuffer();
+import { downloadFile } from './utils';
 
-export const stickerToImage = async (fileBuffer: Buffer) =>
-  sharp(fileBuffer).png().toBuffer();
+export const imageToSticker = async (
+  bot: TelegramBot,
+  chatId: number,
+  fileId: string,
+) => {
+  await bot.sendChatAction(chatId, 'typing');
+
+  try {
+    const fileBuffer = await downloadFile(fileId);
+
+    const transformedFileBuffer = await sharp(fileBuffer)
+      .resize({
+        width: 512,
+        height: 512,
+        fit: 'contain',
+        background: {
+          r: 0,
+          g: 0,
+          b: 0,
+          alpha: 0,
+        },
+      })
+      .webp()
+      .toBuffer();
+
+    await bot.sendSticker(chatId, transformedFileBuffer);
+  } catch (error) {
+    console.error(error);
+    await bot.sendChatAction(chatId, 'typing');
+    await bot.sendMessage(chatId, 'Unsupported image format.');
+  }
+};
+
+export const stickerToImage = async (
+  bot: TelegramBot,
+  chatId: number,
+  fileId: string,
+) => {
+  try {
+    await bot.sendChatAction(chatId, 'upload_document');
+    const fileBuffer = await downloadFile(fileId);
+
+    const transformedFileBuffer = await sharp(fileBuffer).png().toBuffer();
+
+    await bot.sendDocument(
+      chatId,
+      transformedFileBuffer,
+      {},
+      {
+        filename: `${fileId}.png`,
+        contentType: 'image/png',
+      },
+    );
+  } catch (error) {
+    console.error(error);
+    await bot.sendChatAction(chatId, 'typing');
+    await bot.sendMessage(chatId, 'Unsupported image format.');
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { bot, downloadFile } from './utils';
-import { imageToSticker, stickerToImage } from './image';
-
 import type { PhotoSize } from 'node-telegram-bot-api';
+
+import { bot } from './utils';
+import { imageToSticker, stickerToImage } from './image';
 
 bot.on('photo', async ({ chat: { id }, photo = [] }) => {
   const { file_id: largestFileId } = photo.reduce<PhotoSize>(
@@ -10,39 +10,29 @@ bot.on('photo', async ({ chat: { id }, photo = [] }) => {
     { file_id: '', width: 0, height: 0 },
   );
 
-  const fileBuffer = await downloadFile(largestFileId);
+  if (!largestFileId) {
+    return;
+  }
 
-  const transformedFileBuffer = await imageToSticker(fileBuffer);
-
-  await bot.sendSticker(id, transformedFileBuffer);
+  await imageToSticker(bot, id, largestFileId);
 });
 
 bot.on('sticker', async ({ chat: { id }, sticker }) => {
-  if (sticker?.is_animated) {
+  if (!sticker || sticker.is_animated) {
     return;
   }
 
-  const fileId = sticker?.file_id || '';
-
-  const fileBuffer = await downloadFile(fileId);
-
-  const transformedFileBuffer = await stickerToImage(fileBuffer);
-
-  await bot.sendDocument(id, transformedFileBuffer);
+  await stickerToImage(bot, id, sticker.file_id);
 });
 
 bot.on('document', async ({ chat: { id }, document }) => {
-  if (!document?.file_name) {
+  if (!document?.mime_type) {
     return;
   }
 
-  const { file_id: fileId, file_name: fileName } = document;
+  const { file_id: fileId, mime_type: mimeType } = document;
 
-  if (/\.(jpeg|jpg|png|webp|avif|tiff|tif|gif|svg)$/i.test(fileName)) {
-    const fileBuffer = await downloadFile(fileId);
-
-    const transformedFileBuffer = await imageToSticker(fileBuffer);
-
-    await bot.sendSticker(id, transformedFileBuffer);
+  if (mimeType.startsWith('image')) {
+    await imageToSticker(bot, id, fileId);
   }
 });
